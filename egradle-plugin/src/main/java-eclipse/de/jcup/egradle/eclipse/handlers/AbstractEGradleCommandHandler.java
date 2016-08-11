11bd90edbbabe19e767d21532e1e1cda7a91f274
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2016-08-11 07:28:31 +0200

    Remove warnings and made plugin gradle testable
    - removed former used egradle-core library dependency
    - fixed missing generic parts
    - removed unused imports

--- egradle-plugin/src/main/java-eclipse/de/jcup/egradle/eclipse/handlers/AbstractEGradleCommandHandler.java
+++ egradle-plugin/src/main/java-eclipse/de/jcup/egradle/eclipse/handlers/AbstractEGradleCommandHandler.java
@@ -23,9 +23,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.progress.IProgressService;
 
