563da9bc32b177209a9586fa1d21d1be55dbba9a
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2016-10-09 15:06:27 +0200

    initial setup for importer

--- egradle-plugin-main/src/main/java-eclipse/de/jcup/egradle/eclipse/importWizards/EGradleRootProjectImportWizard.java
+++ egradle-plugin-main/src/main/java-eclipse/de/jcup/egradle/eclipse/importWizards/EGradleRootProjectImportWizard.java
@@ -21,19 +21,14 @@ import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IImportWizard;
 import org.eclipse.ui.IWorkbench;
 
-public class ImportWizard extends Wizard implements IImportWizard {
+public class EGradleRootProjectImportWizard extends Wizard implements IImportWizard {
 
 	ImportWizardPage mainPage;
 
-	public ImportWizard() {
+	public EGradleRootProjectImportWizard() {
 		super();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
-	 */
 	public boolean performFinish() {
 		IFile file = mainPage.createNewFile();
 		if (file == null)
@@ -41,23 +36,12 @@ public class ImportWizard extends Wizard implements IImportWizard {
 		return true;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench,
-	 * org.eclipse.jface.viewers.IStructuredSelection)
-	 */
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		setWindowTitle("File Import Wizard"); // NON-NLS-1
+		setWindowTitle("EGradle Import Wizard"); // NON-NLS-1
 		setNeedsProgressMonitor(true);
 		mainPage = new ImportWizardPage("Import Gradle Multiproject", selection); // NON-NLS-1
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.wizard.IWizard#addPages()
-	 */
 	public void addPages() {
 		super.addPages();
 		addPage(mainPage);
