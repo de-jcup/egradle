48c56c28011822af90417d284f7c216768f1fe66
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2016-12-02 18:17:21 +0100

    Added more testcases #130
    - added more testcases
    - changed implementation
    - two tests currently failing

--- egradle-plugin-main/src/main/java/de/jcup/egradle/core/text/SimpleGradleSourceFormatter.java
+++ egradle-plugin-main/src/main/java/de/jcup/egradle/core/text/SimpleGradleSourceFormatter.java
@@ -15,7 +15,10 @@ import java.util.regex.Pattern;
  */
 public class SimpleGradleSourceFormatter {
 
-	private static final String ALL_LINE_SEP_GROUP = "[\\r\\n|\\n\\r]?\\s*";
+	private static final String ALL_LINE_SEP_GROUP = "\\s*[\\r\\n|\\n\\r]?\\s*";
+
+	private static final Pattern REMOVE_TOO_MANY_SPACES = Pattern.compile("[ ]{2,}");
+
 	private static final Pattern REMOVE_LEADING_LINE_BREAKS_CURLYBRACKET_OPEN = Pattern
 			.compile(ALL_LINE_SEP_GROUP + "\\{", Pattern.DOTALL);
 	private static final Pattern REMOVE_LEADING_LINE_BREAKS_CURLYBRACKET_CLOSE = Pattern
@@ -34,7 +37,11 @@ public class SimpleGradleSourceFormatter {
 	public void setConfig(GradleSourceFormatterConfig config) {
 		this.config = config;
 	}
-	/* FIXME ATR, 30.11.2016: normal strings etc. with having { inside will be wrong formatted*/
+
+	/*
+	 * FIXME ATR, 30.11.2016: normal strings etc. with having { inside will be
+	 * wrong formatted
+	 */
 	public GradleSourceFormatterConfig getConfig() {
 		if (config == null) {
 			config = new GradleSourceFormatterConfig();
@@ -48,21 +55,19 @@ public class SimpleGradleSourceFormatter {
 		}
 
 		/* remove all line breaks */
-		text = removeLineBreaksFromCurlyBraces(text);
-
-		/* in each line add anted line breaks */
 		List<String> originLines = transformToLines(text, charset);
 
+		/* in each line add wanted line breaks */
 		StringBuilder withWantedLineBreaks = new StringBuilder();
 		for (String line : originLines) {
-			withWantedLineBreaks.append(addWantedLineBreaks(line));
+			withWantedLineBreaks.append(handleLine(line));
 		}
 		List<String> lines = transformToLines(withWantedLineBreaks.toString(), charset);
 
 		/* remove old indent */
 		List<String> indentLines = buildLinesWithNoWhitespacesBeforeText(lines);
 		/* make indent */
-		indentLines=buildLinesWithIndent(indentLines);
+		indentLines = buildLinesWithIndent(indentLines);
 		StringBuilder sb = new StringBuilder();
 		for (String string : indentLines) {
 			sb.append(string);
@@ -75,33 +80,33 @@ public class SimpleGradleSourceFormatter {
 		int indent = 0;
 
 		for (String line : lines) {
-			GStringParseState state = GStringParseState.NOT_IN_GSTRING;
+			ParseState state = ParseState.NOT_IN_STRING;
 
 			char before = '0';
 			StringBuilder lineWithoutGstrings = new StringBuilder();
 			for (char c : line.toCharArray()) {
 				before = c;
 				if (c == '\"' && before != '\\') {
-					if (state == GStringParseState.IN_GSTRING) {
-						state = GStringParseState.NOT_IN_GSTRING;
+					if (state == ParseState.IN_GSTRING) {
+						state = ParseState.NOT_IN_STRING;
 					} else {
 						/* WAS NOT IN GSTRING */
-						state = GStringParseState.IN_GSTRING;
+						state = ParseState.IN_GSTRING;
 					}
 				}
 				before = c;
-				if (state==GStringParseState.NOT_IN_GSTRING){
+				if (state == ParseState.NOT_IN_STRING) {
 					lineWithoutGstrings.append(c);
 				}
 			}
-			if (lineWithoutGstrings.indexOf("{")!=-1){
-				indentLines.add(indentStr(indent)+line);
+			if (lineWithoutGstrings.indexOf("{") != -1) {
+				indentLines.add(indentStr(indent) + line);
 				indent++;
-			}else if (lineWithoutGstrings.indexOf("}")!=-1){
+			} else if (lineWithoutGstrings.indexOf("}") != -1) {
 				indent--;
-				indentLines.add(indentStr(indent)+line);
-			}else{
-				indentLines.add(indentStr(indent)+line);
+				indentLines.add(indentStr(indent) + line);
+			} else {
+				indentLines.add(indentStr(indent) + line);
 			}
 		}
 		return indentLines;
@@ -127,50 +132,101 @@ public class SimpleGradleSourceFormatter {
 		return indentLines;
 	}
 
-	private String removeLineBreaksFromCurlyBraces(String text) {
-		text = REMOVE_LEADING_LINE_BREAKS_CURLYBRACKET_OPEN.matcher(text).replaceAll("{");
-		text = REMOVE_LEADING_LINE_BREAKS_CURLYBRACKET_CLOSE.matcher(text).replaceAll("}");
-		text = REMOVE_TRAILING_LINE_BREAKS_CURLYBRACKET_OPEN.matcher(text).replaceAll("{");
-		text = REMOVE_TRAILING_LINE_BREAKS_CURLYBRACKET_CLOSE.matcher(text).replaceAll("}");
-		return text;
+	private String removeLineBreaksFromCurlyBraces(String linePart) {
+		linePart = REMOVE_LEADING_LINE_BREAKS_CURLYBRACKET_OPEN.matcher(linePart).replaceAll("{");
+		linePart = REMOVE_LEADING_LINE_BREAKS_CURLYBRACKET_CLOSE.matcher(linePart).replaceAll("}");
+		linePart = REMOVE_TRAILING_LINE_BREAKS_CURLYBRACKET_OPEN.matcher(linePart).replaceAll("{");
+		linePart = REMOVE_TRAILING_LINE_BREAKS_CURLYBRACKET_CLOSE.matcher(linePart).replaceAll("}");
+		return linePart;
 	}
 
-	private enum GStringParseState {
-		IN_GSTRING, NOT_IN_GSTRING
+	private enum ParseState {
+		IN_GSTRING,
+
+		IN_NORMAL_STRING,
+
+		IN_SLASHY_STRING,
+
+		NOT_IN_STRING,
+
 	}
 
-	private String addWantedLineBreaks(String line) {
+	private class FormatterContext {
+		private ParseState state = ParseState.NOT_IN_STRING;
+		private char before = '0';
+		private StringBuilder sb = new StringBuilder();
+	}
+
+	private String handleLine(String line) {
 		StringBuilder result = new StringBuilder();
-		GStringParseState state = GStringParseState.NOT_IN_GSTRING;
 
-		char before = '0';
-		StringBuilder sb = new StringBuilder();
+		FormatterContext context = new FormatterContext();
+
 		char[] charArray = line.toCharArray();
 		for (char c : charArray) {
-			sb.append(c);
-			if (c == '\"' && before != '\\') {
-				if (state == GStringParseState.IN_GSTRING) {
-					result.append(sb.toString());
-					state = GStringParseState.NOT_IN_GSTRING;
-				} else {
-					/* WAS NOT IN GSTRING */
-					result.append(addLineBreaks(sb.toString()));
-					state = GStringParseState.IN_GSTRING;
-				}
-				sb = new StringBuilder();
-			}
-			before = c;
+			context.sb.append(c);
+			handleGString(result, context, c);
+			handleString(result, context, c);
+			context.before = c;
 		}
-		if (state == GStringParseState.NOT_IN_GSTRING) {
-			result.append(addLineBreaks(sb.toString()));
+		if (context.state == ParseState.NOT_IN_STRING) {
+			result.append(removeToMuchSpacesAndAddWantedLineBreaksR(context.sb.toString()));
 		} else {
-			result.append(sb.toString());
+			result.append(context.sb.toString());
 		}
 
 		return result.toString();
 	}
 
-	private String addLineBreaks(String linePart) {
+	private void handleGString(StringBuilder result, FormatterContext context, char c) {
+		if (context.state == ParseState.IN_NORMAL_STRING) {
+			return;
+		}
+		if (c != '\"') {
+			return;
+		}
+		if (context.before == '\\') {
+			return;
+		}
+		if (context.state == ParseState.IN_GSTRING) {
+			result.append(context.sb.toString());
+			context.state = ParseState.NOT_IN_STRING;
+		} else {
+			/* WAS NOT IN GSTRING */
+			result.append(removeToMuchSpacesAndAddWantedLineBreaksR(context.sb.toString()));
+			context.state = ParseState.IN_GSTRING;
+		}
+		context.sb = new StringBuilder();
+	}
+
+	private void handleString(StringBuilder result, FormatterContext context, char c) {
+		if (context.state == ParseState.IN_GSTRING) {
+			return;
+		}
+		if (c != '\'') {
+			return;
+		}
+		if (context.before == '\\') {
+			return;
+		}
+		if (context.state == ParseState.IN_NORMAL_STRING) {
+			result.append(context.sb.toString());
+			context.state = ParseState.NOT_IN_STRING;
+		} else {
+			/* WAS NOT IN GSTRING */
+			result.append(removeToMuchSpacesAndAddWantedLineBreaksR(context.sb.toString()));
+			context.state = ParseState.IN_NORMAL_STRING;
+		}
+		context.sb = new StringBuilder();
+	}
+
+	private String removeToMuchSpacesAndAddWantedLineBreaksR(String linePart) {
+		linePart = REMOVE_TOO_MANY_SPACES.matcher(linePart).replaceAll(" ");
+		linePart = removeLineBreaksFromCurlyBraces(linePart);
+		if (linePart.trim().length() == 0) {
+			return "";
+		}
+
 		/* add wanted line breaks */
 		String openingBracket = getConfig().getBeforeCurlyBracketOpened() + "{"
 				+ getConfig().getAfterCurlyBracketOpened();
--- egradle-plugin-main/src/test/java/de/jcup/egradle/core/text/SimpleGradleSourceFormatterTest.java
+++ egradle-plugin-main/src/test/java/de/jcup/egradle/core/text/SimpleGradleSourceFormatterTest.java
@@ -28,6 +28,78 @@ public class SimpleGradleSourceFormatterTest {
 	}
 	
 	@Test
+	public void method_containing_gstring_with_curlybraces_having_line_breaks__string_is_not_touched() throws Exception{
+		/* @formatter:off*/
+		String code = 
+				"def method(boolean variable1) {\n"+
+				"   if(variable1) {"+
+				"      println(\"hello {\"+\n"+
+				"               \"world}\"\n"+
+				"   }\n"+
+				"   \n"+
+				"}\n";
+		
+		SimpleGradleSourceFormatter f = new SimpleGradleSourceFormatter();
+		String result = f.format(code, "UTF-8");
+
+		String expected = 
+				"def method(boolean variable1) {\n"+
+				"   if(variable1) {\n"+
+				"      println(\"hello {\"+\n"+
+				"      \"world}\"\n"+
+				"   }\n"+
+				"   \n"+
+				"}\n";
+			   
+
+		assertEquals(expected,result);
+
+		/* @formatter:on*/
+	}
+	
+	@Test
+	public void method_containing_if_statement_is_formatted_correct_def__three_spaces_method__is_reduced_to_def_one_space_method() throws Exception{
+		/* @formatter:off*/
+		String code ="def   method(boolean variable1){if(variable1){println('hello world')}}";
+		
+		SimpleGradleSourceFormatter f = new SimpleGradleSourceFormatter();
+		String result = f.format(code, "UTF-8");
+		
+		String expected = 
+		"def method(boolean variable1) {\n"+
+		"   if(variable1) {\n"+
+		"      println('hello world')\n"+
+		"   }\n"+
+		"   \n"+
+		"}\n";
+
+		assertEquals(expected,result);
+
+		/* @formatter:on*/
+	}
+	
+	@Test
+	public void method_containing_if_statement_is_formatted_correct_also_when_many_multiple_linebreaks_direct_after_method_def_followed_with_4_spaces() throws Exception{
+		/* @formatter:off*/
+		String code ="def method(boolean variable1)\n\n\n\n\n\n    {if(variable1){println('hello world')}}";
+		
+		SimpleGradleSourceFormatter f = new SimpleGradleSourceFormatter();
+		String result = f.format(code, "UTF-8");
+		
+		String expected = 
+		"def method(boolean variable1) {\n"+
+		"   if(variable1) {\n"+
+		"      println('hello world')\n"+
+		"   }\n"+
+		"   \n"+
+		"}\n";
+
+		assertEquals(expected,result);
+
+		/* @formatter:on*/
+	}
+	
+	@Test
 	public void method_containing_if_statement_is_formatted_correct_even_when_gstring_contained_with_variable() throws Exception{
 		/* @formatter:off*/
 		String code ="def method(boolean variable1){if(variable1){println(\"variable1 was:${variable1}\")}}";
@@ -39,6 +111,27 @@ public class SimpleGradleSourceFormatterTest {
 		"def method(boolean variable1) {\n"+
 		"   if(variable1) {\n"+
 		"      println(\"variable1 was:${variable1}\")\n"+
+		"   }\n"+
+		"   \n"+
+		"}\n";
+
+		assertEquals(expected,result);
+
+		/* @formatter:on*/
+	}
+	
+	@Test
+	public void method_containing_if_statement_is_formatted_correct_even_when_normal_string_contained_with_curly_brackets() throws Exception{
+		/* @formatter:off*/
+		String code ="def method(boolean variable1){if(variable1){println('testcase for curly braces in normal string: { or } or again { shall be no problem')}}";
+		
+		SimpleGradleSourceFormatter f = new SimpleGradleSourceFormatter();
+		String result = f.format(code, "UTF-8");
+		
+		String expected = 
+		"def method(boolean variable1) {\n"+
+		"   if(variable1) {\n"+
+		"      println('testcase for curly braces in normal string: { or } or again { shall be no problem')\n"+
 		"   }\n"+
 		"   \n"+
 		"}\n";
