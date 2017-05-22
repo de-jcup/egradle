9bfa11544c98dd2a82413f265e1e77e733c2a335
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2017-05-22 13:24:28 +0200

    Fixed license text changes done accidently before #161

--- egradle-plugin-gradle-editor/src/main/java-eclipse/de/jcup/egradle/eclipse/gradleeditor/preferences/GradleEditorPreferenceConstants.java
+++ egradle-plugin-gradle-editor/src/main/java-eclipse/de/jcup/egradle/eclipse/gradleeditor/preferences/GradleEditorPreferenceConstants.java
@@ -5,7 +5,7 @@ import de.jcup.egradle.eclipse.util.PreferenceIdentifiable;
 /*
  * Copyright 2016 Albert Tregnaghi
  *
- * Licensed under the Apache License, VersionData 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *		http://www.apache.org/licenses/LICENSE-2.0
