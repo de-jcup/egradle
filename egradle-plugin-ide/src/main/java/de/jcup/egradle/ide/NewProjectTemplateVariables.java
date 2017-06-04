0d1b1ec9a1d2d81825604c7b8b088b3cdc057f11
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2017-06-04 10:55:22 +0200

    Default templates improved #161
    - src/main and src/test folder now automatically integrated and so
      available after project creation

--- egradle-plugin-ide/src/main/java/de/jcup/egradle/ide/NewProjectTemplateVariables.java
+++ egradle-plugin-ide/src/main/java/de/jcup/egradle/ide/NewProjectTemplateVariables.java
@@ -4,10 +4,11 @@ public enum NewProjectTemplateVariables implements TemplateVariable{
 	
 	VAR__NAME_OF_TEMPLATE ("egradle.template.name"),
 	VAR__NAME_OF_PROJECT ("egradle.template.projectname"),
+	VAR__NAME_OF_GROUP ("egradle.template.groupname"),
 	VAR__NAME_OF_SUBPROJECT ("egradle.template.subprojectname"),
 	VAR__MULTIPROJECTS__INCLUDE_SUBPROJECTS ("egradle.template.subprojects.include"),
-	VAR__JAVA__VERSION("egradle.template.java.version");
-	
+	VAR__JAVA__VERSION("egradle.template.java.version"),
+	;
 	
 	private String variableName;
 
