b6a8362943b7e3e8f33babc0c6eb606373d0e4df
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2017-10-20 12:08:35 +0200

    Changed behaviour so only code blocks are toggled, closes #309
    - when a full comment block, the block will be toggled and comments
      removed
    - when not a full comment block the complete selection got now comments
      before, no matter if comments exist or not
      
    Behaviour should now be like in Java Editor

--- egradle-plugin-main/src/main/java/de/jcup/egradle/core/text/CommentBlockToggler.java
+++ egradle-plugin-main/src/main/java/de/jcup/egradle/core/text/CommentBlockToggler.java
@@ -19,24 +19,46 @@ public class CommentBlockToggler {
 		
 		List<TextLine> toggledLines = new ArrayList<>();
 		int deltaOffset=0;
+		
+		/* when same block we can toggle, otherwise we always comment complete block */
+		boolean toggleEnabled = isSameCommentBlockStart(originLines);
+		
 		for (TextLine originLine: originLines){
 			if (originLine==null){
 				continue;
 			}
 			TextLine toggledLine = null;
 			int originOffset = originLine.getOffset();
-			if (! originLine.getContent().startsWith("//")){
-				String withComment = "//"+originLine.getContent();
-				toggledLine= new TextLine(originOffset+deltaOffset, withComment);
-				deltaOffset+=2;
-			}else{
+			if (toggleEnabled && originLine.getContent().startsWith("//")){
+				/* remove comment */
 				String withoutComment = StringUtilsAccess.substring(originLine.getContent(), 2);
 				toggledLine= new TextLine(originOffset+deltaOffset, withoutComment);
 				deltaOffset-=2;
+			}else{
+				/* add comment to line */
+				String withComment = "//"+originLine.getContent();
+				toggledLine= new TextLine(originOffset+deltaOffset, withComment);
+				deltaOffset+=2;
 			}
 			toggledLines.add(toggledLine);
 		}
 		return toggledLines;
+	}
+
+	boolean isSameCommentBlockStart(List<TextLine> list) {
+		int firstCommentPos =-1;
+		for (TextLine line: list){
+			if (firstCommentPos==-1){
+				firstCommentPos=line.getContent().indexOf("//");
+				continue;
+			}
+			int commentPos = line.getContent().indexOf("//");
+			if (firstCommentPos!=commentPos){
+				// not same block
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	
--- egradle-plugin-main/src/test/java/de/jcup/egradle/core/text/CommentBlockTogglerTest.java
+++ egradle-plugin-main/src/test/java/de/jcup/egradle/core/text/CommentBlockTogglerTest.java
@@ -2,6 +2,7 @@ package de.jcup.egradle.core.text;
 
 import static de.jcup.egradle.core.text.AssertTextLines.*;
 import static java.util.Collections.*;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,51 @@ public class CommentBlockTogglerTest {
 	@Before
 	public void before(){
 		togglerToTest = new CommentBlockToggler();
+	}
+	
+	@Test
+	public void sameBlock__a_b_c__is_true(){
+		List<TextLine> list = new ArrayList<>();
+		list.add(new TextLine(-1,"a"));
+		list.add(new TextLine(-1,"b"));
+		list.add(new TextLine(-1,"c"));
+		
+		/* execute */
+		assertTrue(togglerToTest.isSameCommentBlockStart(list));
+	}
+	
+	@Test
+	public void sameBlock__a_comment_b_c__is_false(){
+		List<TextLine> list = new ArrayList<>();
+		list.add(new TextLine(-1,"a"));
+		list.add(new TextLine(-1,"//b"));
+		list.add(new TextLine(-1,"c"));
+		
+		/* execute */
+		assertFalse(togglerToTest.isSameCommentBlockStart(list));
+	}
+	
+	/**
+	 * <pre>
+	 * 0123        =>    012345 
+	 * //a\n             ////a\n
+	 * 456               678910
+	 * xy\n              //xy\n
+	 * 78                11 12 13 14
+	 * z\n               /  /  z  \n
+	 * </pre>
+	 */
+	@Test
+	public void comment_a_xy_z__converted_correct() {
+		List<TextLine> list = new ArrayList<>();
+		list.add(new TextLine(0,"//a\n"));
+		list.add(new TextLine(4,"xy\n"));
+		list.add(new TextLine(7,"z\n"));
+		/* execute */
+		List<TextLine> lines = togglerToTest.toggle(list);
+		
+		/* test */
+		assertLines(lines).containsOnly(new TextLine(0, "////a\n"),new TextLine(6,"//xy\n"),new TextLine(11,"//z\n"));
 	}
 	
 	/**
