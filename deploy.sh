53e97256e1df8732603b27de673285979647a7a7
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2019-06-07 13:22:02 +0200

    Updated bintray deployment version to 2.6.x

--- deploy.sh
+++ deploy.sh
@@ -16,4 +16,4 @@ if [ -z "$BINTRAY_API_KEY" ]; then
 	echo "bintray api key not set"
 	show_help_and_exit;
 fi
-./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup egradle update-site current ./egradle-updatesite/
+./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup egradle update-site 2.6.x ./egradle-updatesite/
