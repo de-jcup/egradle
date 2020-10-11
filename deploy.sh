7d89edce5ad7643c75fcfabfb86e60bf8a5aec02
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2020-10-12 00:08:29 +0200

    Add jarsign script + changed deployment script , closes #378
    - version must now be given as third parameter
    - signjar adopted from bash-editor plugin signjar mechanism

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BINTRAY_USER=$1
 BINTRAY_API_KEY=$2
+BINTRAY_VERSION=$3
 
 function show_help_and_exit() {
 	echo "Usage:"
@@ -12,8 +13,23 @@ if [ -z "$BINTRAY_USER" ]; then
 	echo "bintray user not set"
 	show_help_and_exit;
 fi
+
 if [ -z "$BINTRAY_API_KEY" ]; then
 	echo "bintray api key not set"
 	show_help_and_exit;
 fi
-./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup egradle update-site 2.6.x ./egradle-updatesite/
+
+if [ -z "$BINTRAY_VERSION" ]; then
+    echo "bintray version not set"
+    show_help_and_exit;
+fi
+
+# call jar signing before pushing to bintray...
+export KEYSTORE_PWD
+export KEYSTORE_LOCATION
+
+set -e
+
+./signJars
+
+./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup egradle update-site ${BINTRAY_VERSION} ./egradle-updatesite/
